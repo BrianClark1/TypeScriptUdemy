3bed37804d9e12f68cfeef28502e9888c5c79abb
Brian Clark <briansclark1@gmail.com>
2022-07-27 17:39:41 -0400

    Finished Classes

--- classes.ts
+++ classes.ts
@@ -61,3 +61,73 @@ class SuperPlayer extends Player {
 
 let elton = new Player("Elton", "Steele");
 // elton.score = '123';
+
+
+
+interface Colorful {
+    color: string; 
+};
+
+interface Printable {
+    print(): void; 
+}
+
+//classes can also interact with interfaces 
+class Bike implements Colorful {
+    constructor(public color: string) {}''
+}
+
+class Jacket implements Colorful, Printable {
+    constructor(public brand: string, public color: string) {};
+
+    print() {
+        console.log(`${this.color} ${this.brand} jacket`);
+    }
+}
+
+const bike1 = new Bike('red');
+
+const jacket1 = new Jacket("Prada", "black");
+
+
+//abstract enforces some type stuff and can also outline functionality like:
+//this class must have an getPay() method , sorta similar to how interfaces require things 
+//abstract classes adds properties and built in methods, 
+//cant instantiate an abstract class only the children that extend it 
+abstract class Employee  {
+    constructor(public first: string, public last: string) {};
+    abstract getPay(): number;
+    greet() {
+        console.log("Hello");
+    }
+};
+
+class FullTimeEmployee extends Employee {
+    constructor(public first: string, last: string, private salary: number) {
+        super(first, last)
+    }
+  getPay() {
+    return this.salary;
+  }
+};
+
+
+class PartTimeEmployee extends Employee {
+  constructor(
+    public first: string,
+    last: string,
+    private hourlyRate: number,
+    private hoursWorked: number
+  ) {
+    super(first, last);
+  }
+  getPay() {
+    return this.hourlyRate * this.hoursWorked;
+  }
+};
+
+
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay);
+
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
