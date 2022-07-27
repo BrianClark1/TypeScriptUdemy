c6710c0f879e3ba05ba5abac3bd9c3de4640dc40
Brian Clark <briansclark1@gmail.com>
2022-07-27 09:20:29 -0400

    JS Classes

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -33,6 +33,7 @@ function handleSubmit(e) {
 function saveTodos() {
     localStorage.setItem("todos", JSON.stringify(todos));
 }
+//TS includes JSON, DOM in the lib by default 
 function createTodo(todo) {
     const newLI = document.createElement("li");
     const checkbox = document.createElement("input");
@@ -48,3 +49,49 @@ function createTodo(todo) {
     newLI.append(checkbox);
 }
 form.addEventListener("submit", handleSubmit);
+//Notes
+//interface Todo {
+//   text: string;
+//   completed: boolean;
+// }
+// const btn = document.getElementById("btn")! as HTMLButtonElement; //Type assertion
+// const input = document.getElementById("todoinput")! as HTMLInputElement;
+// const form = document.querySelector("form")!;
+// const list = document.getElementById("todolist")!;
+// const todos: Todo[] = readTodos();
+// todos.forEach(createTodo);
+// // Load todos from local storage
+// function readTodos(): Todo[] {
+//   const todosJSON = localStorage.getItem("todos");
+//   if (todosJSON === null) return [];
+//   return JSON.parse(todosJSON);
+// }
+// // Save todos to local storage
+// function saveTodos() {
+//   localStorage.setItem("todos", JSON.stringify(todos));
+// }
+// function handleSubmit(e: SubmitEvent) {
+//   e.preventDefault();
+//   const newTodo: Todo = {
+//     text: input.value,
+//     completed: false,
+//   };
+//   createTodo(newTodo);
+//   todos.push(newTodo);
+//   saveTodos();
+//   input.value = "";
+// }
+// function createTodo(todo: Todo) {
+//   const newLI = document.createElement("li");
+//   const checkbox = document.createElement("input");
+//   checkbox.type = "checkbox";
+//   checkbox.checked = todo.completed;
+//   checkbox.addEventListener("change", function () {
+//     todo.completed = checkbox.checked;
+//     saveTodos();
+//   });
+//   newLI.append(todo.text);
+//   newLI.append(checkbox);
+//   list.append(newLI);
+// }
+// form.addEventListener("submit", handleSubmit);
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -46,6 +46,7 @@ function saveTodos() {
       localStorage.setItem("todos", JSON.stringify(todos));
 }
 
+//TS includes JSON, DOM in the lib by default 
 
 function createTodo(todo: Todo) {
   const newLI = document.createElement("li");
@@ -63,3 +64,61 @@ function createTodo(todo: Todo) {
 }
 
 form.addEventListener("submit", handleSubmit);
+
+
+//Notes
+//interface Todo {
+//   text: string;
+//   completed: boolean;
+// }
+
+// const btn = document.getElementById("btn")! as HTMLButtonElement; //Type assertion
+// const input = document.getElementById("todoinput")! as HTMLInputElement;
+// const form = document.querySelector("form")!;
+// const list = document.getElementById("todolist")!;
+
+// const todos: Todo[] = readTodos();
+// todos.forEach(createTodo);
+
+// // Load todos from local storage
+// function readTodos(): Todo[] {
+//   const todosJSON = localStorage.getItem("todos");
+//   if (todosJSON === null) return [];
+//   return JSON.parse(todosJSON);
+// }
+
+// // Save todos to local storage
+// function saveTodos() {
+//   localStorage.setItem("todos", JSON.stringify(todos));
+// }
+
+// function handleSubmit(e: SubmitEvent) {
+//   e.preventDefault();
+//   const newTodo: Todo = {
+//     text: input.value,
+//     completed: false,
+//   };
+//   createTodo(newTodo);
+//   todos.push(newTodo);
+
+//   saveTodos();
+//   input.value = "";
+// }
+
+// function createTodo(todo: Todo) {
+//   const newLI = document.createElement("li");
+//   const checkbox = document.createElement("input");
+//   checkbox.type = "checkbox";
+//   checkbox.checked = todo.completed;
+
+//   checkbox.addEventListener("change", function () {
+//     todo.completed = checkbox.checked;
+//     saveTodos();
+//   });
+
+//   newLI.append(todo.text);
+//   newLI.append(checkbox);
+//   list.append(newLI);
+// }
+
+// form.addEventListener("submit", handleSubmit);
