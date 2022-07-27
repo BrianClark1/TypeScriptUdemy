9fef7c440e59dde3f08dcf49f324f1905e20bae1
Brian Clark <briansclark1@gmail.com>
2022-07-26 21:51:10 -0400

    maniupulating the DOM

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
 console.log('HELLLOOO!!!! HUNGRY YES');
+//Dom is a complicated OBJ, interface that extends etc.
+//If there is no uncertantity at runtime add an exclamation point after getelementById, asserts that its not null, remove the quesiton mark after the button 
+const btn = document.getElementById("btn");
+console.log(btn);
+//Question mark optional chaining operator
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
+    // alert("CLICKED!!")
+    alert(input.value);
+    input.value = "";
+});
+//Type Casting TS isnt smart enough to deduce the specific DOM type 
+const input = document.getElementById("todoinput");
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,1 +1,18 @@
 console.log('HELLLOOO!!!! HUNGRY YES')
+
+
+
+//Dom is a complicated OBJ, interface that extends etc.
+//If there is no uncertantity at runtime add an exclamation point after getelementById, asserts that its not null, remove the quesiton mark after the button 
+const btn = document.getElementById("btn"); 
+console.log(btn);
+
+//Question mark optional chaining operator
+btn?.addEventListener("click", () => {
+    // alert("CLICKED!!")
+    alert(input.value);
+    input.value = "";
+});
+
+//Type Casting TS isnt smart enough to deduce the specific DOM type 
+const input = document.getElementById("todoinput")! as HTMLInputElement;
