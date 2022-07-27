e00a44a0ebf2794592c08d83f63ca51ce614c52c
Brian Clark <briansclark1@gmail.com>
2022-07-26 23:17:57 -0400

    maniupulating the DOM with TS

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,14 +1,33 @@
 "use strict";
-console.log('HELLLOOO!!!! HUNGRY YES');
 //Dom is a complicated OBJ, interface that extends etc.
-//If there is no uncertantity at runtime add an exclamation point after getelementById, asserts that its not null, remove the quesiton mark after the button 
+//If there is no uncertantity at runtime add an exclamation point after getelementById, asserts that its not null, remove the quesiton mark after the button
 const btn = document.getElementById("btn");
-console.log(btn);
 //Question mark optional chaining operator
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
-    // alert("CLICKED!!")
-    alert(input.value);
-    input.value = "";
-});
-//Type Casting TS isnt smart enough to deduce the specific DOM type 
+// btn?.addEventListener("click", () => {
+//     input.value = "";
+// });
+//Type Casting TS isnt smart enough to deduce the specific DOM type
 const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+const todos = [];
+//Specifying this is a Submit Event type, so we can compartmentalize the callback func
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = "";
+}
+function createTodo(todo) {
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLI.append(todo.text);
+    list.append(newLI);
+    newLI.append(checkbox);
+}
+form.addEventListener("submit", handleSubmit);
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,18 +1,44 @@
-console.log('HELLLOOO!!!! HUNGRY YES')
-
-
-
 //Dom is a complicated OBJ, interface that extends etc.
-//If there is no uncertantity at runtime add an exclamation point after getelementById, asserts that its not null, remove the quesiton mark after the button 
-const btn = document.getElementById("btn"); 
-console.log(btn);
+//If there is no uncertantity at runtime add an exclamation point after getelementById, asserts that its not null, remove the quesiton mark after the button
+const btn = document.getElementById("btn");
 
 //Question mark optional chaining operator
-btn?.addEventListener("click", () => {
-    // alert("CLICKED!!")
-    alert(input.value);
-    input.value = "";
-});
+// btn?.addEventListener("click", () => {
+//     input.value = "";
+// });
 
-//Type Casting TS isnt smart enough to deduce the specific DOM type 
+//Type Casting TS isnt smart enough to deduce the specific DOM type
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
+
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
+//Specifying this is a Submit Event type, so we can compartmentalize the callback func
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const newLI = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLI.append(todo.text);
+  list.append(newLI);
+  newLI.append(checkbox);
+}
+
+form.addEventListener("submit", handleSubmit);
