c3561ed11cd5e81138e2ca6d009086b51636ed61
Brian Clark <briansclark1@gmail.com>
2022-07-28 11:02:12 -0400

    completed generics

--- generics.ts
+++ generics.ts
@@ -36,6 +36,116 @@ identity<string>("hello");
 
 
 
+//Takes a list of things, will accept an array of those types and will return an element of that type
+//expect input to be an array of that type 
 function getRandomElement<Type>(list: Type[]): Type {
-
+    const randIdx = Math.floor(Math.random() * list.length);
+    return list[randIdx];
 };
+
+
+console.log(getRandomElement<string>(['a', 'b', 'c'])); 
+
+//TS can infer the type of x is a number
+//the same as let x: number = 23;
+let x = 23;
+
+//TS is smart enough to know that the generic Type is string!
+getRandomElement(['a', 'v', 'avd']);
+//Smart enough to know its a number!
+getRandomElement([1, 2, 3, 4]);
+
+
+
+//Merging 2 objects!
+//This generic function says that it can accept parameters of 2 different types 
+// extends is an addtional constraint telling TS that it needs to be something that is an OBJ 
+function merge<T extends object, U extends object>(object1: T, object2: U) {
+  return {
+    ...object1,
+    ...object2
+  }
+};
+//Avoids TS from assuming an 'any' return type 
+
+const comboObj = merge({ name: "colt" }, { pets: ["blue", "elton"] });
+
+
+//this generic helps you avoid having to explicity define the objects, i.e.
+//merge<{name: string}, {pets: string[]}>
+
+interface Lengthy {
+  length: number;
+}
+
+//Similarly you can explicityly say that the T input type must adhere to the interface 'Lengthy' Properties 
+function printDoubleLength<T extends Lengthy>(thing: T): number {
+  return thing.length * 2;
+}
+
+//Accomplishes the same thing as the following:
+function printDoubleLength(thing: Lengthy): number {
+  return thing.length * 2;
+}
+
+
+//Default type parameters 
+function makeEmptyArray<T = number>(): T[] {
+  return [];
+};
+
+//Check the type of nums, it defaults to number 
+const nums = makeEmptyArray();
+//However if we define the type as boolean it no longer defualts as number 
+const bools = makeEmptyArray<boolean>();
+
+
+//Classes with Generics 
+
+interface Song {
+  title: string;
+  artist: string;
+}
+
+interface Video {
+  title: string;
+  creator: string;
+  resolution: string;
+}
+
+class VideoPlaylist {
+  public videos: Video[] = [];
+}
+
+class SongPlaylist {
+  public songs: Song[] = [];
+}
+
+
+//Now we have a generic Class called Playlist 
+//which can be used to create a queue of videos or songs, since its a generic 
+class Playlist<T> {
+  public queue: T[] = [];
+  add(el: T) {
+    this.queue.push(el);
+  }
+}
+
+const songs = new Playlist<Song>();
+const videos = new Playlist<Video>();
+
+//Element parameter is expted to be a video 
+videos.add();
+
+
+
+
+
+
+
+
+
+
+
+
+
